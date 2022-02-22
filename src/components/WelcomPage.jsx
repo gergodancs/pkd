22b09ac1f58f2e7bccefc0c7ddf53c0a9bc14ed0
gergodancs <dancs.gergely@gmail.com>
2022-02-22 12:44:33 +0100

    dani

--- src/components/WelcomPage.jsx
+++ src/components/WelcomPage.jsx
@@ -4,8 +4,44 @@ import { Link } from "react-router-dom";
 
 const WelcomPage = () => {
   return (
-    <div className="wlc-wrapper">
-      <Link to="/next">WelcomPage</Link>
+    <div>
+      <img className="logo" src={require("./pics/pkdlogo.png")} alt="" />
+      <div className="wlc-wrapper">
+        <img
+          className="justice"
+          src={require("./pics/justice.png")}
+          alt="justice"
+        />
+        <div className="wlc-szoveg">
+          <h2>Welcome</h2>
+          <div className="p">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
+              facilis nihil itaque labore sunt eveniet provident autem delectus.
+              Dolores voluptate vitae necessitatibus labore. Recusandae illum,
+              nihil perferendis aliquam delectus rerum. Ipsum quo incidunt
+              temporibus, magni minima dolorem cum recusandae aliquid quibusdam
+              itaque aut natus consequatur illo eaque provident quia molestiae.
+              Molestiae quidem quasi architecto unde natus ducimus in aliquam
+              repellendus! Odio, non dolorum porro sunt at autem vitae ex ab
+              vero. Reiciendis commodi consequuntur nobis quia quae natus id ab,
+              necessitatibus dicta nam distinctio ratione exercitationem
+              corrupti vero error saepe.
+            </p>
+          </div>
+          <Link to="/next">
+            <h2 className="enter">Enter</h2>
+          </Link>
+        </div>
+
+        <div className="wlc-img">
+          <img
+            className="danipics"
+            src={require("./pics/daniwlc.jpg")}
+            alt="dani"
+          />
+        </div>
+      </div>
     </div>
   );
 };
